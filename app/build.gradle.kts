9933d35dbb89d1213d788ada821f42bcad589065
bogdan.mashchenko <bogdan.mashchenko@corp.mail.ru>
2024-07-05 16:09:14 +0300

    upgraded version to 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     kotlin("android")
-    id("ru.ok.tracer") version("0.2.13")
+    id("ru.ok.tracer") version("0.3.1")
 }
 
 android {
@@ -58,17 +58,17 @@ dependencies {
     // которые необходимы в данный момент.
 
     // Сбор и анализ крешей и ANR
-    implementation("ru.ok.tracer:tracer-crash-report:0.2.13")
+    implementation("ru.ok.tracer:tracer-crash-report:0.3.1")
     // Сбор и анализ нативных крешей
-    implementation("ru.ok.tracer:tracer-crash-report-native:0.2.13")
+    implementation("ru.ok.tracer:tracer-crash-report-native:0.3.1")
     // Сбор и анализ хипдапмов при OOM
-    implementation("ru.ok.tracer:tracer-heap-dumps:0.2.13")
+    implementation("ru.ok.tracer:tracer-heap-dumps:0.3.1")
     // Анализ потребления дискового места на устройстве
-    implementation("ru.ok.tracer:tracer-disk-usage:0.2.13")
+    implementation("ru.ok.tracer:tracer-disk-usage:0.3.1")
     // Семплирующий профайлер
-    implementation("ru.ok.tracer:tracer-profiler-sampling:0.2.13")
+    implementation("ru.ok.tracer:tracer-profiler-sampling:0.3.1")
     // Систрейс
-    implementation("ru.ok.tracer:tracer-profiler-systrace:0.2.13")
+    implementation("ru.ok.tracer:tracer-profiler-systrace:0.3.1")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
